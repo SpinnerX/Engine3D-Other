458a8561580a62547cd3baa5cf801fb2ef5b518e
SpinnerX <heraaronhotmail@yahoo.com>
2024-04-23 17:31:43 -0800

    Updated shaders

--- Resources/shaders/basics/Red.glsl
+++ Resources/shaders/basics/Red.glsl
@@ -5,8 +5,10 @@
 // In this case location of this attribute is at index 0
 layout(location = 0) in vec3 position;
 
+// Something to Note is that gl_Position is vec4 by default!
+// If we convert position to vec3 then we get unexpected compilation error with this shader
 void main(){
-    gl_Position = position;
+    gl_Position = vec4(position, 1.0);
 }
 
 #type fragment
